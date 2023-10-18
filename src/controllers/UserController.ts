96ed0ed6dfbf4bce20a24356f4e92c576c08613e
Kevin Souza <kevinwillyansouza@gmail.com>
2023-10-18 10:12:07 -0400

    feat: criação do CRUD do user completa

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -15,6 +15,20 @@ async function indexUser(req: Request, res: Response) {
     }
 }
 
+async function indexUserById(
+    req: Request<{ id?: UpdateWithAggregationPipeline }>,
+    res: Response
+) {
+    const { id } = req.params;
+
+    try {
+        const user = await User.findById(id, "-senha");
+        return res.status(200).json({ user });
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
+}
+
 async function storeUser(req: Request, res: Response) {
     const { nome, senha, email } = req.body;
 
@@ -41,4 +55,57 @@ async function storeUser(req: Request, res: Response) {
     }
 }
 
-export { indexUser, storeUser };
+async function updateUser(
+    req: Request<{ id?: UpdateWithAggregationPipeline }>,
+    res: Response
+) {
+    const { nome, senha, email } = req.body;
+    const { id } = req.params;
+
+    if (!nome && !senha && !email) {
+        return res.status(400).json({ error: "You must enter a new data" });
+    }
+
+    let encryptedPassword;
+
+    if (senha) {
+        encryptedPassword = await bcrypt.hash(senha, 8);
+    }
+
+    const filter = { _id: id };
+    const updateDoc = {
+        $set: {
+            nome,
+            senha: encryptedPassword,
+            email,
+        },
+    };
+
+    try {
+        await User.updateOne(filter, updateDoc);
+
+        return res.status(200).json({ message: "User updated succesfully!" });
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
+}
+
+async function deleteUser(
+    req: Request<{ id?: UpdateWithAggregationPipeline }>,
+    res: Response
+) {
+    const { id } = req.params;
+    const filter = { _id: id };
+
+    try {
+        const user = await User.deleteOne(filter);
+        if (user.deletedCount < 1) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        return res.status(200).json({ message: "User removed succesfully!" });
+    } catch (err) {
+        return res.status(500).json({ error: err });
+    }
+}
+
+export { indexUser, indexUserById, storeUser, updateUser, deleteUser };
--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,16 @@
 import express from "express";
-import { indexUser, storeUser } from "./controllers/UserController";
+import {
+    deleteUser,
+    indexUser,
+    indexUserById,
+    storeUser,
+    updateUser,
+} from "./controllers/UserController";
 
 export const routes = express.Router();
 
 routes.get("/users", indexUser);
+routes.get("/users/:id", indexUserById);
 routes.post("/users", storeUser);
+routes.put("/users/:id", updateUser);
+routes.delete("/users/:id", deleteUser);
