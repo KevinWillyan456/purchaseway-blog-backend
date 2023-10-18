5c796e28f7c5ac9f4f8491996307c5b82bc39090
Kevin Souza <kevinwillyansouza@gmail.com>
2023-10-18 07:49:49 -0400

    feat: criação do arquivo de rotas

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import { config } from "dotenv";
 import cors from "cors";
 import { connectToDatabase } from "./database";
+import { routes } from "./routes";
 const app = express();
 const port = 3000;
 
@@ -11,10 +12,7 @@ connectToDatabase();
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-app.get("/", (req: Request, res: Response) => {
-    res.send("Hello World");
-});
+app.use(routes);
 
 app.listen(port, () =>
     console.log(`Servidor rodando na porta: ${port} - http://localhost:${port}`)
