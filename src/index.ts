52fc810457752c30e13a2ef2d855eb5655af4351
Kevin Souza <kevinwillyansouza@gmail.com>
2023-10-18 07:47:45 -0400

    chore: configurando o banco de dados

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import express, { Request, Response } from "express";
+import { config } from "dotenv";
+import cors from "cors";
+import { connectToDatabase } from "./database";
 const app = express();
 const port = 3000;
+
+config();
+connectToDatabase();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World");
