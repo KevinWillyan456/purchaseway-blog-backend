b4a1529030282f51897d06056f346204a491e1be
Kevin Souza <kevinwillyansouza@gmail.com>
2024-02-12 16:20:03 -0400

    feat: criação de autenticação de acesso pode chave de API

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,21 @@ import { config } from "dotenv";
 import cors from "cors";
 import { connectToDatabase } from "./database";
 import { routes } from "./routes";
+import AuthAPI from "./middlewares/AuthAPI";
+
 const app = express();
-const port = 3000;
 
 config();
 connectToDatabase();
 
 app.use(cors());
+app.use(AuthAPI);
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(routes);
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () =>
     console.log(`Servidor rodando na porta: ${port} - http://localhost:${port}`)
---
+++ src/middlewares/AuthAPI.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from "express";
+
+export default function AuthAPI(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+    const apiKey = req.headers["authorization"];
+
+    if (!apiKey || apiKey !== `${process.env.API_KEY || "none"}`) {
+        return res
+            .status(401)
+            .json({ mensagem: "API Key invalid or not present" });
+    }
+
+    next();
+}
